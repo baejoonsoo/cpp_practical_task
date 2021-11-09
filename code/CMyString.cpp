2517a353b37c3efe99071015e4c99acc4c258d58
baejoonsoo <baejoonsoo21@gmail.com>
2021-11-09 22:13:26 +0900

    save

--- code/CMyString.cpp
+++ code/CMyString.cpp
@@ -64,3 +64,73 @@ CMyString::CMyString(CMyString&& rhs){
     rhs.m_pszData=nullptr;
     rhs.m_nLength=0;
 }
+
+int CMyString::GetLength() const{
+    return m_nLength;
+}
+
+int CMyString::Append(const char* pszParam){
+	// 매개변수 유효성 검사
+	if (pszParam == nullptr)
+		return 0;
+
+	// 현재 문자열의 길이 백업
+	int nLenCur = m_nLength;
+
+	// 매개변수 문자열 길이
+	int nLenParam = strlen(pszParam);
+	
+	if(nLenParam == 0)
+		return 0;
+
+	// m_pszData 에 문자열이 없다면 새로 문자열을 할당한 것과 동일하게 처리함
+	if (m_pszData == nullptr)
+	{
+		SetString(pszParam);
+		return m_nLength;
+	}
+
+	// 두 문자열을 합쳐서 저장할 수 있는 메모리를 새로 할당함
+	char* pszResult = new char[nLenCur + nLenParam + 1];
+
+	// 문자열 조합
+	strcpy(pszResult, m_pszData);
+	strcpy(pszResult + nLenCur, pszParam);
+
+	// 기존 문자열 삭제 및 멤버 정보 갱신
+	Release();
+
+	m_pszData = pszResult;
+	m_nLength = nLenCur + nLenParam;
+
+	return m_nLength;
+}
+
+CMyString& CMyString::operator=(CMyString&& rhs){
+  // 얕은 복사를 수행해도 상관 없다. 어차피 곧 원본이 소멸되기 때문
+	this->m_nLength = rhs.m_nLength;
+	this->m_pszData = rhs.m_pszData;
+
+  // 원본 임시 객체의 멤버들은 초기화 한다. 절대로 해제하면 안된다.
+	rhs.m_nLength = 0;
+	rhs.m_pszData = nullptr;
+
+	return *this;
+}
+
+
+CMyString CMyString::operator+(const CMyString& rhs){
+    CMyString temp;
+    temp.m_nLength=m_nLength+rhs.m_nLength;
+    temp.m_pszData=new char[temp.m_nLength+1];
+
+    strcpy(temp,m_pszData);
+    strcpy(temp+m_nLength,rhs.m_pszData);
+
+    return temp;
+}
+
+CMyString& CMyString::operator+=(const CMyString& rhs){
+    Append(rhs);
+    return *this;
+}
--- code/CMyString.h
+++ code/CMyString.h
@@ -3,13 +3,19 @@
 
 class CMyString 
 {
-public:  // 생성자, 소멸자, 복사 생성자
+public:  // 생성자, 소멸자 / 연산자
 	CMyString();
 	~CMyString();
 	CMyString(const CMyString& rhs);			// 복사 생성자
+	CMyString(CMyString&& rhs); // 이동 생성자
+	explicit CMyString(const char* str);	// 변환 생성자
+
 	CMyString& operator=(const CMyString& rhs);	// 대입 연산자
+	CMyString& operator=(CMyString&& rhs);
+	CMyString operator+(const CMyString& rhs);
+	CMyString& operator+=(const CMyString& rhs);
+
 	
-	explicit CMyString(const char* str);
 	operator char*() const;
 private:
 	// 문자열을 저장하기 위해 동적 할당한 메모리를 가리키는 포인터
@@ -19,8 +25,9 @@ private:
 public:  // my method
 	int SetString(const char* pszParam);
 	const char* GetString() const;
-		CMyString(CMyString&& rhs); // 이동 생성자
 	void Release();
+	int GetLength() const;
+	int Append(const char* pszParam);
 };
 
 #endif
